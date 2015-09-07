970cb8acb50ee2a5649a1fd8d6f81a1bd8ed635d
Jan Krems <jan.krems@groupon.com>
2015-09-06 17:39:36 -0700

    Basic error handling for spawn-server

---
+++ test/processes/proxy.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var path = require('path');
+
+var tap = require('tap');
+
+var Config = require('../../lib/config');
+var Proxy = require('../../lib/processes/proxy');
+var spawnServer = require('../../lib/spawn-server');
+
+tap.test('Proxy.getOptions', function(t) {
+  var config = new Config({ app: { port: 3041 } });
+  Proxy.getOptions(config)
+    .then(function(options) {
+      t.ok(options.port, 'Finds an open port for the proxy');
+      t.equal(options.args[1], '3041',
+        'Passes in the app port as the 2nd param to the child');
+      t.end();
+    }, function(error) {
+      t.error(error);
+      t.end();
+    });
+});
+
+tap.test('Launching the proxy', function(t) {
+  var config = new Config({
+    root: path.resolve(__dirname, '../tmp/proxy'),
+    app: { port: 3041 }
+  });
+  spawnServer(config, Proxy)
+    .then(function(proxy) {
+      proxy.kill();
+      t.end();
+    })
+    .then(null, function(error) {
+      t.error(error);
+      t.end();
+    });
+});
--- test/spawn-server.js
+++ test/spawn-server.js
@@ -16,12 +16,57 @@ test('spawn a simple node http server', function(t) {
       return {
         command: process.execPath,
         args: [ 'examples/hello-world/server.js', 'Robin' ],
-        port: 3040
+        port: 3000
       };
     }
   }).then(function(child) {
     t.type(child.pid, 'number', 'has a numeric pid');
     child.kill();
+    t.end();
+  });
+});
+
+test('a child that fails to start', function(t) {
+  spawnServer(new Config({
+    root: __dirname + '/tmp/server'
+  }), {
+    name: 'throws',
+    getOptions: function() {
+      return {
+        command: process.execPath,
+        args: [ 'examples/throws/server.js', 'Robin' ],
+        port: 3040
+      };
+    }
+  }).then(function(child) {
+    t.fail('Should have failed b/c the child exits');
+    child.kill();
+    t.end();
+  }, function(error) {
+    console.log(error);
+    t.end();
+  });
+});
+
+test('a child that takes too long to listen', function(t) {
+  spawnServer(new Config({
+    root: __dirname + '/tmp/server'
+  }), {
+    name: 'hello-world',
+    getOptions: function() {
+      return {
+        command: process.execPath,
+        args: [ 'examples/hello-world/server.js', 'Robin' ],
+        timeout: 250,
+        port: 3001 // wrong port on purpose
+      };
+    }
+  }).then(function(child) {
+    t.fail('Should have failed b/c the child listens on the wrong port');
+    child.kill();
+    t.end();
+  }, function(error) {
+    console.log(error);
     t.end();
   });
 });
