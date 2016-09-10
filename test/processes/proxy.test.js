9bd66424bea8798eef197c083aa10039193e3ae7
David Bushong <dbushong@groupon.com>
2016-09-09 19:24:40 -0700

    feat: use proxy.host for remote selenium grid
    * if `proxy.host` is set, it is used in URLs instead of 127.0.0.1
    * if it is not set, but `selenium.serverUrl` is, it defaults to `hostname -f`
    * this should allow network-reachable testium invocations to use selenium on a different host

--- test/processes/proxy.test.js
+++ test/processes/proxy.test.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import cp from 'child_process';
 
 import assert from 'assertive';
 
@@ -6,6 +7,10 @@ import Config from '../../lib/config';
 import App from '../../lib/processes/application';
 import Proxy from '../../lib/processes/proxy';
 import spawnServer from '../../lib/spawn-server';
+
+import Bluebird from 'bluebird';
+
+Bluebird.promisifyAll(cp);
 
 const HELLO_WORLD = path.resolve(__dirname, '../../examples/hello-world');
 
@@ -17,6 +22,18 @@ describe('Proxy', () => {
     assert.equal('Passes in the app port as the 2nd param to the child',
       '3041', options.commandArgs[1]);
     assert.equal('http://127.0.0.1:' + options.port, config.get('proxy.targetUrl'));
+  });
+
+  it('can generate remote-selenium spawn options', async () => {
+    const config = new Config(
+      { app: { port: 3041 }, selenium: { serverUrl: 'http://example.com' } }
+    );
+    const [options, hostname] = await Bluebird.all([
+      Proxy.getOptions(config),
+      cp.execFileAsync('hostname', ['-f'], { encoding: 'utf8' }).call('trim'),
+    ]);
+    assert.equal(`http://${hostname}:${options.port}`,
+                 config.get('proxy.targetUrl'));
   });
 
   it('can actually spawn', async () => {
